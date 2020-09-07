4f8f08a54b17ed5f586b0fabb44fd4591b4fd9ca
bibinlittle <56598909+bibinlittle@users.noreply.github.com>
2020-09-08 00:01:42 +0530

    about content change

--- my_portfolio/src/components/PageFragments/HomePage/AboutMe.jsx
+++ my_portfolio/src/components/PageFragments/HomePage/AboutMe.jsx
@@ -6,16 +6,14 @@ import { stripTags, domHtml } from '../../../utils/stripTags';
 import SEO from '../../Seo';
 
 const pageText = {
-  paraOne: `Hello !! My name is Rolwin Reevan Monteiro. I'm a full stack web developer who is
-    passionate about various web technologies. I like to experiment with different web
-    technologies. I have an experience of nearly 3 years working with LAMP stack, MERN stack
-    and ELK stack. Building fancy UI's just like this one that your seeing 😅 and writing blogs about tech stacks
-    is what Rolwin loves to do. Check my blog which I update every week for some Javascript and some
-    cool notes on web technologies.`,
-  paraTwo: `Currently I work mostly with Javascript technologies like ReactJS and NodeJS. I also
+  paraOne: `Hello !! My name is Bibin Little. I'm an entrepreneur & a full stack web developer who is
+    passionate about various web technologies.I have an experience of nearly 3 years working with LAMP stack, MERN stack
+    and ELK stack.I love to Building fancy UI's and writing blogs about futer tech.
+    Check my portfolio to know more about my works.`,
+  paraTwo: `Currently I work mostly with Javascript technologies like ReactJS , NodeJS , Gatsby & Flutter. I also
     have hands on experience working with cloud infrastructures like <b>AWS/GCP</b> and have deployed applications
     keeping scalability in mind. Docker, Kubernetes, Jenkins, SonarQube are some of the cool
-    tools I use for <b>CI/ CD</b>. I'm always a learner and a self taught programmer.`,
+    tools I use. I'm always a learner and a self taught programmer.`,
 };
 
 const AboutMe = () => {
@@ -42,31 +40,31 @@ const AboutMe = () => {
             height={60}
             alt="location image"
             textH4="Born and bought up in"
-            textH3="Mangalore, KA, India"
+            textH3="Trivandrum, Kerala, India"
           />
         </Col>
         <Col xs={24} sm={24} md={12} lg={8}>
           <AboutTile
             img="coffee.png"
             alt="coffee image"
-            textH4="Love Coffee"
-            textH3="Coffee + Me = Happiness"
+            textH4="Love Meetups"
+            textH3="Coffee + Meetups = Happiness"
           />
         </Col>
         <Col xs={24} sm={24} md={12} lg={8}>
           <AboutTile
             img="meeting.png"
             alt="meeting image"
-            textH4="Socially Awkward"
-            textH3="At times"
+            textH4="Loves to connect"
+            textH3="Loves to network and connect with new people"
           />
         </Col>
         <Col xs={24} sm={24} md={12} lg={8}>
           <AboutTile
             img="motorcycle.png"
             alt="motorcycle image"
-            textH4="Love Riding"
-            textH3="Biker for life"
+            textH4="Born hodophile"
+            textH3="Find new places see new faces"
           />
         </Col>
         <Col xs={24} sm={24} md={12} lg={8}>
@@ -74,7 +72,7 @@ const AboutMe = () => {
             img="web.png"
             alt="web image"
             textH4="Self Taught Programmer"
-            textH3="Thanks to the Web Resources"
+            textH3="Thanks to the Colt Steel, Angelina Yu etc"
             height={60}
             width={60}
           />
@@ -83,8 +81,9 @@ const AboutMe = () => {
           <AboutTile
             img="graduation.png"
             alt="graduation image"
-            textH4="Pursued B.Tech in"
-            textH3="Computer Science"
+            textH4="Jack of all trades"
+
+            textH3="B.Tech in Civil Eng ,       MA English Lit"
             height={60}
             width={60}
           />
